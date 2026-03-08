c5b5a7a4b6fd71ba1b63e3fe1e50f04616b88b58
Edwardo <edwardo.rivera11@gmail.com>
2026-03-07 21:43:14 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py 
+DATABASE = {
+    "host": "mydb.acme.internal",
+    "user": "admin",
+    "password": "secret",   # hardcoded credential
+    "dbname": "banking_prod"
+}
+
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
